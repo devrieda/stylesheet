36a8893398495683bc9148aab2c406ac42e877a4
Derek DeVries <devrieda@gmail.com>
2013-04-10 11:39:26 -0600

    fill out document specs and add parsing for styles

--- lib/stylesheet/document.rb
+++ lib/stylesheet/document.rb
@@ -1,4 +1,58 @@
 module Stylesheet
   class Document
+    attr_accessor :url
+    
+    def initialize(url)
+      @url = url
+    end
+
+    def location
+      @location ||= Location.new(url)
+    end
+
+    def text 
+      @text ||= request_content
+    end
+
+    def style_sheets
+      @style_sheets ||= StyleSheetList.new(styles)
+    end
+
+
+    private
+
+    def styles 
+      (inline_styles + external_styles).each do |style|
+         { parent:  self,
+           content: style.inner_html, 
+           href:    style["href"],
+           media:   style["media"],
+           title:   style["title"],
+           type:    style["type"] }
+      end
+    end
+
+    # find all inline styles and build new stylesheet from them
+    def inline_styles
+      parser.css("style").map {|style| style }
+    end
+
+    def external_styles
+      parser.css("link[rel='stylesheet']").map {|style| style }
+    end
+    
+    def request_content
+      raise InvalidLocationError unless location.valid?
+
+      request.get(location.href)
+    end
+
+    def parser
+      @parser ||= Nokogiri::HTML(text)
+    end
+
+    def request
+      @request ||= Stylesheet.request
+    end
   end
 end
--- spec/document_spec.rb
+++ spec/document_spec.rb
@@ -1,45 +1,89 @@
 require 'spec_helper'
 
 describe Document do 
+  before(:each) do 
+    Stylesheet.request = FakeRequest.new
+  end
+
+  describe "#location" do 
+    it "should build a new Location object from the url" do 
+      url = "http://example.com/css/absolute.html"
+      doc = Document.new(url)
+
+      expect(doc.location).to be_kind_of(Location)
+      expect(doc.location.href).to eq url
+    end
+
+    it "should build location from invalid url" do 
+      doc = Document.new("asdf")
+      expect(doc.location).to be_kind_of(Location)
+      expect(doc.location.href).to eq "/asdf"
+    end
+  end
+
+  describe "#text" do 
+    it "should request text for document body" do 
+      doc = Document.new("http://example.com/css/absolute.html")
+
+      expect(doc.text).to match /DOCTYPE html/
+    end
+
+    it "should thow error if host is invalid" do 
+      doc = Document.new("http://")
+      expect {
+        doc.text
+      }.to raise_error(InvalidLocationError)
+    end
+
+    it "should thow error if protocol is invalid" do 
+      doc = Document.new("foo.com")
+      expect {
+        doc.text
+      }.to raise_error(InvalidLocationError)
+    end
+  end
+
   describe "#style_sheets" do 
-    before do 
-      @request = FakeRequest.new
+    it "parses a style sheet list collection from absolute path" do
+      doc = Document.new("http://example.com/css/absolute.html")
+
+      expect(doc.style_sheets).to be_kind_of(StyleSheetList)
+      expect(doc.style_sheets.length).to eq 2
     end
 
-    it "parse a style sheet list collection from absolute path" do
-      text = @request.get("http://example.com/css/absolute.html")
-      # doc  = Document.new(text)
-      # doc.style_sheet_list.should be_kind_of(StyleSheetList)
+    it "parses a style sheet list collection from inline path" do
+      doc = Document.new("http://example.com/css/inline.html")
+
+      expect(doc.style_sheets).to be_kind_of(StyleSheetList)
+      expect(doc.style_sheets.length).to eq 2
     end
 
-    it "parse a style sheet list collection from inline path" do
-      text = @request.get("http://example.com/css/inline.html")
-      # doc  = Document.new(text)
-      # .should be_kind_of(StyleSheetList)
-    end
-    
-    it "parse a style sheet list collection from inline import path" do
-      text = @request.get("http://example.com/css/inline_import.html")
-      # doc  = Document.new(text)
-      # .should be_kind_of(StyleSheetList)
+    it "parses a style sheet list collection from inline import path" do
+      doc = Document.new("http://example.com/css/inline_import.html")
+
+      expect(doc.style_sheets).to be_kind_of(StyleSheetList)
+      expect(doc.style_sheets.length).to eq 1
     end
 
-    it "parse a style sheet list collection from invalid path" do
-      text = @request.get("http://example.com/css/invalid.html")
-      # doc  = Document.new(text)
-      # .should be_kind_of(StyleSheetList)
+    it "parses a style sheet list collection from invalid path" do
+      doc = Document.new("http://example.com/css/invalid.html")
+
+      expect(doc.style_sheets).to be_kind_of(StyleSheetList)
+      expect(doc.style_sheets.length).to eq 2
     end
 
-    it "parse a style sheet list collection from relative path" do
-      text = @request.get("http://example.com/css/relative.html")
-      # doc  = Document.new(text)
-      # .should be_kind_of(StyleSheetList)
-    end
+    it "parses a style sheet list collection from relative path" do
+      doc = Document.new("http://example.com/css/relative.html")
 
-    it "parse a style sheet list collection from relative root path" do
-      text = @request.get("http://example.com/css/relative_root.html")
-      # doc  = Document.new(text)
-      # .should be_kind_of(StyleSheetList)
+      expect(doc.style_sheets).to be_kind_of(StyleSheetList)
+      expect(doc.style_sheets.length).to eq 2
+    end
+ 
+    it "parses a style sheet list collection from relative root path" do
+      doc = Document.new("http://example.com/css/relative_root.html")
+
+      expect(doc.style_sheets).to be_kind_of(StyleSheetList)
+      expect(doc.style_sheets.length).to eq 2
     end
   end
 end
