c07f496de0804122f19f191836c69dd75264ea73
Derek DeVries <devrieda@gmail.com>
2013-04-12 12:41:03 -0600

    add style sheet list specs, functionality

--- lib/stylesheet/style_sheet_list.rb
+++ lib/stylesheet/style_sheet_list.rb
@@ -1,4 +1,12 @@
 module Stylesheet
-  class StyleSheetList
+  class StyleSheetList < Array
+    def initialize(styles)
+      @styles = styles.map {|args| CssStyleSheet.new(args) }
+      super @styles
+    end
+
+    def item(index)
+      self[index]
+    end
   end
 end
--- spec/style_sheet_list_spec.rb
+++ spec/style_sheet_list_spec.rb
@@ -1,21 +1,46 @@
 require 'spec_helper'
 
 describe StyleSheetList do 
+  before(:each) do 
+    Stylesheet.request = FakeRequest.new
+  end
+  
+  let(:stylesheets) do 
+    StyleSheetList.new([
+      { href:  "http://example.com/css/stylesheets/colors.css", media: "screen" }, 
+      { content: "  div {\n    background-color: #aaa;\n    border: 1px solid #ccc;\n  }"}, 
+    ])
+  end
+  
+  describe ".new" do 
+    it "should initialize a new style sheet list " do 
+      expect(stylesheets).to be_kind_of(StyleSheetList)
+    end
+  end
+  
   describe "#[]" do 
     it "finds a css style sheet at the given index" do 
-      # .should be_kind_of(CSSStyleSheet)
+      expect(stylesheets[0]).to be_kind_of(CssStyleSheet)
+    end
+  end
+  
+  describe "#each" do 
+    it "iterates over each style sheet" do 
+      i = 0
+      stylesheets.each {|stylesheet| i += 1 }
+      expect(i).to eq 2
     end
   end
   
   describe "#length" do 
     it "returns number of items" do 
-      
+      expect(stylesheets.length).to eq 2
     end
   end
   
   describe "#item" do 
     it "finds a css style sheet at the given index" do 
-      # .should be_kind_of(CSSStyleSheet)
+      expect(stylesheets.item(0)).to be_kind_of(CssStyleSheet)
     end
   end
 end
