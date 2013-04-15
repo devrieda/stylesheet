a7f193ae8b010eac6b063cb82586b5930c34de20
Derek DeVries <devrieda@gmail.com>
2013-04-15 10:33:14 -0600

    formatting

--- spec/style_sheet_list_spec.rb
+++ spec/style_sheet_list_spec.rb
@@ -4,13 +4,13 @@ describe StyleSheetList do
   before(:each) do 
     Stylesheet.request = FakeRequest.new
   end
-  
+
   let(:external) do 
     { href: "http://example.com/css/stylesheets/colors.css", media: "screen" }
   end
 
   let(:inline) do 
-    { content: "div {\n  background-color: #aaa;\n  border: 1px solid #ccc;\n}"}
+    { content: "div {\n  background-color: #aaa;\n  border: 1px solid #ccc;\n}" }
   end
 
   describe ".new" do 
@@ -19,14 +19,14 @@ describe StyleSheetList do
       expect(stylesheets).to be_kind_of(StyleSheetList)
     end
   end
-  
+
   describe "#[]" do 
     it "finds a css style sheet at the given index" do 
       stylesheets = StyleSheetList.new([external, inline])
       expect(stylesheets[0]).to be_kind_of(CssStyleSheet)
     end
   end
-  
+
   describe "#each" do 
     it "iterates over each style sheet" do 
       stylesheets = StyleSheetList.new([external, inline])
@@ -36,14 +36,14 @@ describe StyleSheetList do
       expect(i).to eq 2
     end
   end
-  
+
   describe "#length" do 
     it "returns number of items" do 
       stylesheets = StyleSheetList.new([external, inline])
       expect(stylesheets.length).to eq 2
     end
   end
-  
+
   describe "#item" do 
     it "finds a css style sheet at the given index" do 
       stylesheets = StyleSheetList.new([external, inline])
