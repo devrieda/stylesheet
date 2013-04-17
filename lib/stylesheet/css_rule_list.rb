86fcb5bba34d0abb1c82ca48b46ab86e2f5aa92f
Derek DeVries <devrieda@gmail.com>
2013-04-17 08:31:05 -0600

    start parsing of rules

--- lib/stylesheet/css_rule_list.rb
+++ lib/stylesheet/css_rule_list.rb
@@ -5,11 +5,24 @@ module Stylesheet
     include Enumerable
 
     def initialize(rules)
-      @rules = []
+      @rules = parse(rules)
     end
 
     def item(index)
       @rules[index]
+    end
+    
+    private
+    
+    def parse(rules)
+      # clean extraneous whitespace
+      rules = rules.to_s.gsub("\n", "").gsub(/\s+/, " ").gsub(/([\};])\s/, '\1')
+
+      directive_re = "@.+?;"
+      rules_re     = ".+?\{.+?\}"
+      split_rules = rules.scan(/(#{directive_re}|#{rules_re})/im).map {|r| r[0] }
+
+      split_rules.map {|css_text| CssRule.factory(:css_text => css_text) }
     end
   end
 end
--- spec/css_rule_list_spec.rb
+++ spec/css_rule_list_spec.rb
@@ -1,9 +1,69 @@
 require 'spec_helper'
 
 describe CssRuleList do 
+  
+  let(:styles) do 
+    "@charset \"UTF-8\";
+
+    body {
+      color: #444;
+      background-color: #535353;
+    }
+
+    @import url(\"import1.css\");
+
+    a:link { 
+      color: #357ad1; 
+    }
+    a:visited { 
+      color: #77abf0; 
+    }
+
+    @media only screen and (max-width: 850px) {
+      font-weight: bold;
+    }
+    
+    @font-face {
+      font-family: \"Bitstream Vera Serif Bold\";
+      src: url(\"http://example.com/fonts/VeraSeBd.ttf\");
+    }
+
+    a:hover {
+      color: #333;
+    }"
+  end
+  
+  describe ".new" do 
+    it "parses charset rules" do 
+      rules = CssRuleList.new(styles)
+      expect(rules[0]).to be_kind_of(CssCharsetRule)
+    end
+    
+    it "parses import rules" do 
+      rules = CssRuleList.new(styles)
+      expect(rules[2]).to be_kind_of(CssImportRule)
+    end
+    
+    it "parses font face rules" do 
+      rules = CssRuleList.new(styles)
+      expect(rules[6]).to be_kind_of(CssFontFaceRule)
+    end
+    
+    it "parses media rules" do 
+      rules = CssRuleList.new(styles)
+      expect(rules[5]).to be_kind_of(CssMediaRule)
+    end
+    
+    it "parses style rules" do 
+      rules = CssRuleList.new(styles)
+      expect(rules[1]).to be_kind_of(CssStyleRule)
+    end
+  end
+  
   describe "#[]" do 
     it "finds a css style sheet at the given index" do 
-      # .to be_kind_of(CSSStyleRule)
+      rules = CssRuleList.new(styles)
+      expect(rules[0]).to be_kind_of(CssRule)
     end
   end
 
