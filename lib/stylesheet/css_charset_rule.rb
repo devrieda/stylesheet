4edd097b6222b3459396b0efee00f65b25dd1705
Derek DeVries <devrieda@gmail.com>
2013-04-18 11:31:35 -0600

    add css charset rule subclass of css rule

--- lib/stylesheet/css_charset_rule.rb
+++ lib/stylesheet/css_charset_rule.rb
@@ -1,4 +1,26 @@
 module Stylesheet
   class CssCharsetRule < CssRule
+    
+    def initialize(args)
+      super
+    end
+    
+    def type
+      CssRule::CHARSET_RULE
+    end
+    
+    def encoding
+      ""
+    end
+
+    def self.matches_rule?(text)
+      text.include?("@charset")
+    end
+    
+    private
+    
+    def parse_css_text
+      
+    end
   end
 end
--- spec/css_charset_rule_spec.rb
+++ spec/css_charset_rule_spec.rb
@@ -1,4 +1,33 @@
 require 'spec_helper'
 
 describe CssCharsetRule do
+
+  let(:css_text) { "@charset \"UTF-8\";" }
+
+  let(:rule) do 
+    CssCharsetRule.new(:css_text => css_text)
+  end
+
+  describe "#type" do 
+    it "shows the type of style rule" do 
+      expect(rule.type).to eq CssRule::CHARSET_RULE
+    end
+  end
+  
+  describe ".matches_rule?" do 
+    it "should match text starting with @charset" do 
+      matches = CssCharsetRule.matches_rule?(css_text)
+      expect(matches).to be_true
+    end
+    
+    it "should not match text without at-rule" do 
+      matches = CssCharsetRule.matches_rule?("a:link { color: #357ad1; }")
+      expect(matches).to be_false
+    end
+
+    it "should not match text without charset" do 
+      matches = CssCharsetRule.matches_rule?("@import url(\"import1.css\");")
+      expect(matches).to be_false
+    end    
+  end
 end
