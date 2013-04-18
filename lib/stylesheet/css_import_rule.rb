d9fddb277742e227ed21cf85349d79702163ed32
Derek DeVries <devrieda@gmail.com>
2013-04-18 11:32:09 -0600

    add css import rule subclass of css rule

--- lib/stylesheet/css_import_rule.rb
+++ lib/stylesheet/css_import_rule.rb
@@ -1,4 +1,30 @@
 module Stylesheet
   class CssImportRule < CssRule
+
+    def initialize(args)
+      super
+    end
+    
+    def type
+      CssRule::IMPORT_RULE
+    end
+    
+    def href
+      ""
+    end
+    
+    def style_sheet
+      
+    end
+
+    def self.matches_rule?(text)
+      text.include?("@import")
+    end
+
+    private
+
+    def parse_css_text
+      
+    end
   end
 end
--- spec/css_import_rule_spec.rb
+++ spec/css_import_rule_spec.rb
@@ -1,4 +1,32 @@
 require 'spec_helper'
 
 describe CssImportRule do
+  let(:css_text) { "@import url(\"import1.css\");" }
+
+  let(:rule) do 
+    CssImportRule.new(:css_text => css_text)
+  end
+
+  describe "#type" do 
+    it "shows the type of style rule" do 
+      expect(rule.type).to eq CssRule::IMPORT_RULE
+    end
+  end
+  
+  describe ".matches_rule?" do 
+    it "should match text starting with @import" do 
+      matches = CssImportRule.matches_rule?(css_text)
+      expect(matches).to be_true
+    end
+    
+    it "should not match text without at-rule" do 
+      matches = CssImportRule.matches_rule?("a:link { color: #357ad1; }")
+      expect(matches).to be_false
+    end
+
+    it "should not match text without import" do 
+      matches = CssImportRule.matches_rule?("@charset \"UTF-8\";")
+      expect(matches).to be_false
+    end
+  end
 end
