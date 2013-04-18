4d1371052582b44046ad17518b4281e245b89c6f
Derek DeVries <devrieda@gmail.com>
2013-04-18 11:31:52 -0600

    add css font face rule subclass of css rule

--- lib/stylesheet/css_font_face_rule.rb
+++ lib/stylesheet/css_font_face_rule.rb
@@ -1,4 +1,26 @@
 module Stylesheet
   class CssFontFaceRule < CssRule
+
+    def initialize(args)
+      super
+    end
+    
+    def type
+      CssRule::FONT_FACE_RULE
+    end
+
+    def style
+      CssStyleDeclaration.new
+    end
+
+    def self.matches_rule?(text)
+      text.include?("@font-face")
+    end
+
+    private
+    
+    def parse_css_text
+      
+    end
   end
 end
--- spec/css_font_face_rule_spec.rb
+++ spec/css_font_face_rule_spec.rb
@@ -1,4 +1,44 @@
 require 'spec_helper'
 
 describe CssFontFaceRule do
+
+  let(:css_text) do 
+    "@font-face {
+      font-family: \"Bitstream Vera Serif Bold\";
+      src: url(\"http://example.com/fonts/VeraSeBd.ttf\");
+    }" 
+  end
+
+  let(:rule) do 
+    CssFontFaceRule.new(:css_text => css_text)
+  end
+
+  describe "#style" do 
+    it "returns the css style declaration for the rule" do 
+      expect(rule.style).to be_kind_of(CssStyleDeclaration)
+    end
+  end
+
+  describe "#type" do 
+    it "shows the type of style rule" do 
+      expect(rule.type).to eq CssRule::FONT_FACE_RULE
+    end
+  end
+  
+  describe ".matches_rule?" do 
+    it "should match text starting with @font-face" do 
+      matches = CssFontFaceRule.matches_rule?(css_text)
+      expect(matches).to be_true
+    end
+    
+    it "should not match text without at-rule" do 
+      matches = CssFontFaceRule.matches_rule?("a:link { color: #357ad1; }")
+      expect(matches).to be_false
+    end
+
+    it "should not match text without font-face" do 
+      matches = CssFontFaceRule.matches_rule?("@import url(\"import1.css\");")
+      expect(matches).to be_false
+    end
+  end
 end
