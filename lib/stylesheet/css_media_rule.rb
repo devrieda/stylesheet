043eb0b817a43872670235d0e72f51222b9f3d76
Derek DeVries <devrieda@gmail.com>
2013-04-18 11:32:28 -0600

    add css media rule subclass of css rule

--- lib/stylesheet/css_media_rule.rb
+++ lib/stylesheet/css_media_rule.rb
@@ -1,4 +1,34 @@
 module Stylesheet
   class CssMediaRule < CssRule
+    
+    attr_reader :content
+
+    def initialize(args)
+      super
+    end
+    
+    def type
+      CssRule::MEDIA_RULE
+    end
+
+    def css_rules
+      @css_rules ||= CssRuleList.new(content)
+    end
+
+    def self.matches_rule?(text)
+      text.include?("@media")
+    end
+    
+    private
+    
+    def parse_css_text
+      return unless css_text.include?("{")
+
+      @content = if matches = css_text.match(/\{(.*)\}/mi)
+        matches[1]
+      else
+        ""
+      end
+    end
   end
 end
--- spec/css_media_rule_spec.rb
+++ spec/css_media_rule_spec.rb
@@ -1,4 +1,49 @@
 require 'spec_helper'
 
 describe CssMediaRule do
+
+  let(:css_text) do
+    "@media only screen and (max-width: 850px) {
+      #main .section {
+        font-weight: bold;
+      }
+      #main a:link {
+        text-decoration: none;
+      }
+    }"
+  end
+
+  let(:rule) do 
+    CssMediaRule.new(:css_text => css_text)
+  end
+
+  describe "#css_rules" do 
+    it "should find child rules" do 
+      expect(rule.css_rules).to be_kind_of CssRuleList
+      expect(rule.css_rules.length).to eq 2
+    end
+  end
+
+  describe "#type" do 
+    it "shows the type of style rule" do 
+      expect(rule.type).to eq CssRule::MEDIA_RULE
+    end
+  end
+  
+  describe ".matches_rule?" do 
+    it "should match text starting with @media" do 
+      matches = CssMediaRule.matches_rule?(css_text)
+      expect(matches).to be_true
+    end
+
+    it "should not match text without at-rule" do 
+      matches = CssMediaRule.matches_rule?("a:link { color: #357ad1; }")
+      expect(matches).to be_false
+    end
+
+    it "should not match text without media" do 
+      matches = CssMediaRule.matches_rule?("@charset \"UTF-8\";")
+      expect(matches).to be_false
+    end
+  end
 end
