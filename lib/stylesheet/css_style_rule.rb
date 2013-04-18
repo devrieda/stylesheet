370bbcc4d8101d67fd2928cdf1fa570ce035261b
Derek DeVries <devrieda@gmail.com>
2013-04-18 11:29:28 -0600

    add css style rule subclass of css rule

--- lib/stylesheet/css_style_rule.rb
+++ lib/stylesheet/css_style_rule.rb
@@ -1,4 +1,32 @@
 module Stylesheet
-  class CssStyleRule
+  class CssStyleRule < CssRule
+
+    attr_reader :selector_text
+
+    def initialize(args)
+      @parent_rule = args[:parent_rule]
+      super
+    end
+    
+    def type
+      CssRule::STYLE_RULE
+    end
+    
+    def style
+      CssStyleDeclaration.new
+    end
+
+    def self.matches_rule?(text)
+      !text.include?("@")
+    end
+
+    private
+
+    def parse_css_text
+      return unless css_text.include?("{")
+
+      selector, declarations = css_text.split("{")
+      @selector_text = selector.strip
+    end
   end
 end
--- spec/css_style_rule_spec.rb
+++ spec/css_style_rule_spec.rb
@@ -1,39 +1,42 @@
 require 'spec_helper'
 
 describe CssStyleRule do 
-  describe "#css_text" do 
-    it "returns full css text for the given rule" do 
-
-    end
+  
+  let(:css_text) do 
+    "#main .body a:link { font-weight: bold; text-decoration: none }"
   end
 
-  describe "#parent_rule" do 
-    it "refers back to the parent rule" do 
-
-    end
-  end
-
-  describe "#parent_style_sheet" do 
-    it "refers back to the parent style sheet" do 
-      # .to be_kind_of(CSSStyleSheet)
-    end
+  let(:rule) do 
+    CssStyleRule.new(:css_text => css_text)
   end
 
   describe "#selector_text" do 
     it "returns only selector text for the style rule" do 
-
+      expect(rule.selector_text).to eq "#main .body a:link"
     end
   end
-  
+
   describe "#style" do 
     it "returns the css style declaration for the rule" do 
-      # .to be_kind_of(CSSStyleDeclaration)
+      expect(rule.style).to be_kind_of(CssStyleDeclaration)
     end
   end
   
   describe "#type" do 
     it "shows the type of style rule" do 
-
+      expect(rule.type).to eq CssRule::STYLE_RULE
+    end
+  end
+  
+  describe ".matches_rule?" do 
+    it "should match text that doesn't begin with an at-rule" do 
+      matches = CssStyleRule.matches_rule?(css_text)
+      expect(matches).to be_true
+    end
+    
+    it "should not match rules starting with at-rule" do 
+      matches = CssStyleRule.matches_rule?("@import url(\"import1.css\");")
+      expect(matches).to be_false
     end
   end
 end
