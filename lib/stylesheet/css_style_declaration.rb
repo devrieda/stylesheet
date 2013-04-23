e61c89c888e65023712b3a7d32d1d4e470927fa0
Derek DeVries <devrieda@gmail.com>
2013-04-23 10:08:18 -0600

    start implementing style declarations

--- lib/stylesheet/css_style_declaration.rb
+++ lib/stylesheet/css_style_declaration.rb
@@ -1,4 +1,29 @@
 module Stylesheet
   class CssStyleDeclaration
+    extend Forwardable
+    def_delegators :@declarations, :length, :[], :each, :<<, :push, :delete
+    include Enumerable
+
+    attr_reader :parent_rule
+
+    def initialize(options={})
+      @parent_rule  = options[:parent_rule]
+      self.css_text = options[:css_text]
+    end
+
+    def css_text=(css_text)
+      @declarations = css_text.to_s.split(";").map {|d| d.strip }
+    end
+
+    def css_text
+      css_text = @declarations.join("; ")
+      css_text += ";" if css_text != ""
+    end
+
+    alias_method :to_s, :css_text
+    
+    def method_missing
+      
+    end
   end
 end
--- spec/css_style_declaration_spec.rb
+++ spec/css_style_declaration_spec.rb
@@ -1,33 +1,56 @@
 require 'spec_helper'
 
 describe CssStyleDeclaration do 
+  let(:css_text) do 
+    "color: #444;
+     font-size: 12px;
+     font-family: Arial, Verdana;
+     border-left: 1px solid red;
+     border-right-width: 1px;
+     background-color: #535353"
+  end
+
+  let(:rule) do 
+    CssStyleRule.new(:css_text => "div.section { #{css_text} }")
+  end
+  
+  let(:declaration) do 
+    CssStyleDeclaration.new(:css_text => css_text, :parent_rule => rule)
+  end
+
+  
+  describe "#length" do 
+    it "returns number of declarations" do 
+      expect(declaration.length).to eq 6
+    end
+  end
+
   describe "#[]" do 
     it "finds name of a declaration for given index" do 
-      
+      expect(declaration[1]).to eq "font-size: 12px"
     end
   end
 
   describe "#css_text" do 
     it "builds the css text from the declarations" do 
-      
+      expected = "color: #444; font-size: 12px; font-family: Arial, Verdana; " + 
+                 "border-left: 1px solid red; border-right-width: 1px; " + 
+                 "background-color: #535353;"
+
+      expect(declaration.css_text).to eq expected
     end
   end
-  
-  describe "#length" do 
-    it "returns number of declarations" do 
-      
-    end
-  end
+
   
   describe "#parent_rule" do 
     it "returns the parent css style rule" do 
-      # .to be_kind_of(CSSStyleRule)
+      expect(declaration.parent_rule).to eq rule
     end
   end
 
   describe "a declaration" do 
     it "finds a declaration value by name" do 
-
+      
     end
   end
 end
