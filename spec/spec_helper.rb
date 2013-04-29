396fad0dce064bfedf32e20fcd252bbe6faadf3f
Derek DeVries <devrieda@gmail.com>
2013-04-29 09:22:42 -0600

    clean up unused code

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,4 @@ require_relative '../spec/stubs/fake_request.rb'
 include Stylesheet
 
 RSpec.configure do |config|
-  # enable filtering for examples
-  # config.filter_run :wip => nil
-  # config.run_all_when_everything_filtered = true
 end
